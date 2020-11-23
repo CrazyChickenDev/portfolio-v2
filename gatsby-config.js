4f21024fe626318137c1158a74a375f3754e754e
CrazyChickenDev <dannychukz15@gmail.com>
2020-11-23 23:48:16 +0100

    edited jobs

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,8 +19,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'NwaobiDaniel',
+        short_name: 'NwaobiDaniel',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
