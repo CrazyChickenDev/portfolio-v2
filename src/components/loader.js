d57ec5446496b9c449c34725ec0d7611b4020523
CrazyChickenDev <dannychukz15@gmail.com>
2020-11-23 02:53:27 +0100

    edited loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -51,7 +51,7 @@ const Loader = ({ finishLoading }) => {
         strokeDashoffset: [anime.setDashoffset, 0],
       })
       .add({
-        targets: '#logo #B',
+        targets: '#logo #D',
         duration: 700,
         easing: 'easeInOutQuart',
         opacity: 1,
