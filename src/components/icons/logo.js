12c25648ee83cc33c509ccdfe0c55a6ab2a4f546
CrazyChickenDev <dannychukz15@gmail.com>
2020-11-24 09:15:21 +0100

    fixed logo.js

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -1,4 +1,4 @@
-/*import React from 'react';
+import React from 'react';
 
 const IconLogo = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
@@ -23,4 +23,3 @@ const IconLogo = () => (
 );
 
 export default IconLogo;
-*/
