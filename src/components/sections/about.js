d48283a4bc29d0125e303e7acf3ac2dbe75d7a79
0x00CCSec <Dannychukz15@gmail.com>
2021-07-10 17:44:50 +0100

    update

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -116,7 +116,7 @@ const StyledPic = styled.div`
 const About = () => {
   const data = useStaticQuery(graphql`
     query {
-      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "me.jpg" }) {
+      avatar: file(sourceInstanceName: { eq: "images" }, relativePath: { eq: "me.png" }) {
         childImageSharp {
           fluid(maxWidth: 500, traceSVG: { color: "#64ffda" }) {
             ...GatsbyImageSharpFluid_withWebp_tracedSVG
