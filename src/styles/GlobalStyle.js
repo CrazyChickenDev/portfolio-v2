bdd1959bda7e32e7895a2b59e76abe461851657f
CrazyChickenDev <Dannychukz15@gmail.com>
2022-09-07 14:54:44 +0100

    revamp

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -9,15 +9,15 @@ const GlobalStyle = createGlobalStyle`
   :root {
     --dark-navy: #F6F4F4;
     --navy: #F6F4F4;
-    --light-navy: #2C2727;
-    --lightest-navy: #3E3939;
+    --light-navy: #FFFFFF;
+    --lightest-navy: #2C2727;
     --navy-shadow: rgba(2, 12, 27, 0.7);
     --slate: #8892b0;
     --light-slate: #a8b2d1;
     --lightest-slate: #ccd6f6;
     --white: #e6f1ff;
     --green: #FF7517;
-    --green-tint: rgba(100, 255, 218, 0.1);
+    --green-tint: rgba(255, 135, 57, 0.1);
 
     --font-sans: 'Calibre', 'San Francisco', 'SF Pro Text', -apple-system, system-ui, sans-serif;
     --font-mono: 'SF Mono', 'Fira Code', 'Fira Mono', 'Roboto Mono', monospace;
