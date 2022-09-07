38d64f156a3592999d00d39c2586ffa1173edadb
CrazyChickenDev <Dannychukz15@gmail.com>
2022-09-07 15:35:35 +0100

    revamp

--- src/components/nav.js
+++ src/components/nav.js
@@ -30,7 +30,7 @@ const StyledHeader = styled.header`
     css`
       height: var(--nav-scroll-height);
       transform: translateY(0px);
-      background-color: rgba(10, 25, 47, 0.85);
+      background-color: rgba(255, 135, 57, 0.85);
       box-shadow: 0 10px 30px -10px var(--navy-shadow);
     `};
 
