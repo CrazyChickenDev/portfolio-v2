e9d9e75db0c7992cd0826f598f3c2863f206e31b
CrazyChickenDev <Dannychukz15@gmail.com>
2023-05-14 04:44:41 +0100

    new

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -153,7 +153,7 @@ const About = () => {
             <p>
               I'm a Computer Engineer, Web Developer, Designer,{' '}
               <a href="https://githubcampus.expert/CrazyChickenDev/" target="_blank">GitHub Campus Expert</a> and{' '}
-              <a href="https://linkedin/in/nwaobidaniel/" target="_blank">Web3 Enthusiast</a> based in
+              <a href="https://linkedin/in/nwaobidaniel/" target="_blank">Blockchain Enthusiast</a> based in
               Benin City, NG.
             </p>
 
